089549f91b4024117b5c8a17feebc44bc0720d4d
ritapessoa01 <ritappessoa@gmail.com>
2024-11-15 18:26:54 +0000

    Added basic JavaScript of the search engine (no API)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function citySearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", citySearch);
