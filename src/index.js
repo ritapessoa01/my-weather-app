14f7130e7c8434a789a1528bfdeb8ea026b894ce
ritapessoa01 <ritappessoa@gmail.com>
2024-11-16 01:14:27 +0000

    Integrated API in some base elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+
+  let countryElement = document.querySelector("#weather-app-country");
+  countryElement.innerHTML = response.data.country;
+}
+
+function updateInfo(city) {
+  let apiKey = "43465c40f4c5d3a735b5b8tbed06aoae";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function citySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  updateInfo(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
